3b3915ccfc8946ad2bed8f44dba5b92e34364055
viktor <vktr.buyukli@gmail.com>
2023-06-03 21:58:23 +0300

    update static

--- color_extractor/views.py
+++ color_extractor/views.py
@@ -4,11 +4,11 @@ from color_extractor.services import extract_colors
 
 
 def index(request):
-    #img_url = "http://surl.li/eyopf"
+    # img_url = "http://surl.li/eyopf"
 
-    img_url = "https://file.liga.net/images/general/2020/09/08/20200908171549-5386.jpg?v=1599578314"
-    img_name = "test_image.jpg"
-    extract_colors(img_url, img_name)
+    # img_url = "https://file.liga.net/images/general/2020/09/08/20200908171549-5386.jpg?v=1599578314"
+    # img_name = "test_image.jpg"
+    # extract_colors(img_url, img_name)
 
     context = "Color Extraction page"
     return render(request, "index.html", {"context": context})
