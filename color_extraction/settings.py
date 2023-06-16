9e9a0fece81398f2d04a6c24d391a5438cb330a2
viktor <vktr.buyukli@gmail.com>
2023-06-16 18:34:33 +0300

    add postgres

--- color_extraction/settings.py
+++ color_extraction/settings.py
@@ -1,6 +1,10 @@
 import os
 from pathlib import Path
 
+# import environ
+import dj_database_url
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,12 +82,22 @@ WSGI_APPLICATION = "color_extraction.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+DATABASES = {"default": dj_database_url.config(conn_max_age=600, ssl_require=True)}
+
+
+# env = environ.Env()
+# environ.Env.read_env(".env")
+#
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": env("POSTGRES_NAME"),
+#         "USER": env("POSTGRES_USER"),
+#         "PASSWORD": env("POSTGRES_PASSWORD"),
+#         "HOST": env("POSTGRES_HOST"),
+#         "PORT": env("POSTGRES_PORT")
+#     }
+# }
 
 
 # Password validation
--- color_extractor/models.py
+++ color_extractor/models.py
@@ -1,1 +1,5 @@
-# from django.db import models
+from django.db import models
+
+
+class ImageExtraction(models.Model):
+    image_name = models.CharField(max_length=255)
