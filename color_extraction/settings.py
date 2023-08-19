b6a631452ed923282c0656eeffc9c831463bf89f
viktor <vktr.buyukli@gmail.com>
2023-08-19 23:09:57 +0300

    add examples

--- color_extraction/settings.py
+++ color_extraction/settings.py
@@ -76,12 +76,8 @@ else:
 
     DATABASES = {
         "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": env("POSTGRES_NAME"),
-            "USER": env("POSTGRES_USER"),
-            "PASSWORD": env("POSTGRES_PASSWORD"),
-            "HOST": env("POSTGRES_HOST"),
-            "PORT": env("POSTGRES_PORT"),
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
         }
     }
 
